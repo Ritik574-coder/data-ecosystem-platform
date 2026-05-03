e7cdd83bd877fba92e0c66e88754d7415064a0b5
ritik574-coder <ritik74820@gmail.com>
2026-05-03 23:02:01 +0530

    addin some data cleaning in custoer tanble

--- Data Warehouse/BusinessDW/Script/Silver/DataPrifiling/cust_info.sql
+++ Data Warehouse/BusinessDW/Script/Silver/DataPrifiling/cust_info.sql
@@ -11,34 +11,43 @@ SELECT TOP (1000) [cst_id]
       ,[cst_create_date]
   FROM [BusinessDW].[Bronze].[crm_cust_info]
 
-SELECT TOP 100 * FROM Bronze.crm_prd_info ;
-SELECT TOP 100 * FROM Bronze.crm_sales_details ;
-
-SELECT TOP 100 * FROM Bronze.erp_cust_az12 ;
-SELECT TOP 100 * FROM Bronze.erp_loc_a101 ;
-SELECT TOP 100 * FROM Bronze.erp_px_cat_g1v2 ;
---=================================================================================
---===================CSUTOEMR ID PROFILING CHECKS==================================
---=================================================================================
--- DUPLICATE CHECK IN CUSTOEMR ID
+--===============================================================================
+--======================= NULL and duplicate finding ============================
+--===============================================================================
 SELECT 
     cst_id,
-    COUNT(cst_id) as customer_id_count
-FROM Bronze.crm_cust_info 
+    COUNT(*) as customer_count
+FROM Bronze.crm_cust_info
 GROUP BY cst_id 
-HAVING COUNT(*) > 1 ;
+HAVING COUNT(*) > 1 OR cst_id IS NULL ;
 
--- NULL CHECK IN CUSTOMER ID 
+--===============================================================================
+--============================ check fresh info  ================================
+--===============================================================================
 SELECT 
     *
 FROM Bronze.crm_cust_info
-WHERE cst_id IS NULL ;
---=================================================================================
---===================CSUTOEMR FIRST NAME PROFILING CHECKS==========================
---=================================================================================
+WHERE cst_id = 29466 ;
 
--- NULL CHECK IN CUSTOMER FIRST NAME
+--===============================================================================
+--============================ check fresh info  ================================
+--===============================================================================
 SELECT 
-    * 
-FROM Bronze.crm_cust_info
-WHERE cst_firstname IS NULL ;
+    *
+FROM
+(
+    SELECT 
+        *,
+        ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS last_flag 
+    FROM Bronze.crm_cust_info
+    WHERE cst_id IS NOT NULL
+)t WHERE last_flag = 1 
+
+--===============================================================================
+--========================= cleaning space first name  ==========================
+--===============================================================================
+SELECT 
+    cst_firstname,
+    TRIM(cst_firstname) AS FirstName
+FROM Bronze.crm_cust_info 
+WHERE cst_firstname != TRIM(cst_firstname);
